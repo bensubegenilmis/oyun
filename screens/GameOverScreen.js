4fb09bcc6a565f22933e1c1032ee47e90c7bef74
Bensu Beğenilmiş <bensubegenilmiss@gmail.com>
2024-02-09 17:37:14 +0300

    overscreencss

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,12 +1,49 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , Image} from 'react-native';
 import React from 'react';
+import Title from '../components/Title';
+import CustomButton from '../components/CustomButton';
+
 
 export default function GameOverScreen() {
   return (
-    <View>
-      <Text>GameOverScreen</Text>
+    <View style={styles.container}>
+      <Title>Congratulations!</Title>
+      <View style={styles.imageview}>
+        <Image style={styles.image} source = {require('../assets/gameover.png')} />
+      </View>
+      <Text style={styles.result}>
+        <Text style={styles.countAndNumber}> 10 </Text>denemeyle
+        <Text style={styles.countAndNumber}> 50 </Text>sayısını buldun..
+      </Text>
+      <CustomButton>Yeni Oyuna Başla</CustomButton>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    flex: 1,
+    alignItems:'center',
+    justifyContent:'center',
+
+  },
+  image:{
+    borderRadius:30,
+  
+  },
+  imageview:{
+    margin:30,
+  },
+  result:{
+    fontSize:20,
+    textAlign:'center',
+    marginBottom: 20,
+
+  },
+  countAndNumber:{
+    color: 'yellow',
+
+  },
+ 
+});
